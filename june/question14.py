c3c361791e2a90c0ca61a81b283429f765cce881
sanskreate <123264981+sanskreate@users.noreply.github.com>
2024-06-30 18:40:08 +0530

    Create question14.py

---
+++ june/question14.py
@@ -0,0 +1,39 @@
+'''14. Longest Common Prefix
+
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "". 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+'''
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""        
+        prefix = strs[0]        
+        for string in strs[1:]:   
+            while string[:len(prefix)] != prefix:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        
+        return prefix
+
+solution = Solution()
+print(solution.longestCommonPrefix(["flower","flow","flight"])) 
+print(solution.longestCommonPrefix(["dog","racecar","car"]))    
