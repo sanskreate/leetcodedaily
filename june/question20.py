c3a282a9d9df3a0cdb5f9268d5065f384e593d1e
sanskreate <123264981+sanskreate@users.noreply.github.com>
2024-06-30 18:37:27 +0530

    Create question20.py

---
+++ june/question20.py
@@ -0,0 +1,45 @@
+'''20. Valid Parentheses
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+Constraints:
+
+1 <= s.length <= 10^4
+s consists of parentheses only '()[]{}'.
+'''
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []  
+        mapping = {')': '(', '}': '{', ']': '['}
+        for char in s:          
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:       
+                stack.append(char)
+        return not stack
+
+solution = Solution()
+print(solution.isValid("()"))       
+print(solution.isValid("()[]{}"))  
+print(solution.isValid("(]"))       
