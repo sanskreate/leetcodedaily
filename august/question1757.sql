0c08ed990fc569d058b9f3b5e3ea8d4b2b8d33ab
sanskreate <123264981+sanskreate@users.noreply.github.com>
2024-08-09 22:46:55 +0530

    Update question1757.sql

--- august/question1757.sql
+++ august/question1757.sql
@@ -1,12 +1,8 @@
 """1757. Recyclable and Low Fat Products
-Solved
-Easy
-Topics
-Companies
 SQL Schema
 Pandas Schema
+ 
 Table: Products
-
 +-------------+---------+
 | Column Name | Type    |
 +-------------+---------+
@@ -17,18 +13,12 @@ Table: Products
 product_id is the primary key (column with unique values) for this table.
 low_fats is an ENUM (category) of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
 recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
- 
 
 Write a solution to find the ids of products that are both low fat and recyclable.
-
 Return the result table in any order.
-
 The result format is in the following example.
 
- 
-
 Example 1:
-
 Input: 
 Products table:
 +-------------+----------+------------+
@@ -49,7 +39,7 @@ Output:
 +-------------+
 Explanation: Only products 1 and 3 are both low fat and recyclable."""
 
-
+ 
 Select product_id 
 From Products 
 Where low_fats = "Y" And recyclable = "Y";
